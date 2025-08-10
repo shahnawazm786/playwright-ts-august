f9abf04130687dff7a1f7d87336d4590c4cf2d65
Mohammad Shahnawaz <msnazia2012@gmail.com>
2025-08-10 09:29:04 +0530

    initial commit

---
+++ playwright.config.ts
@@ -0,0 +1,5 @@
+import { defineConfig, devices } from "@playwright/test";
+
+const config = {
+  testDir: "./tests",
+};
---
+++ tests/demo.spec.ts
@@ -0,0 +1,8 @@
+import { test } from "@playwright/test";
+
+test("demo", async ({ page }) => {
+  await page.goto("https://www.google.com");
+});
+test("demo1", async ({ page }) => {
+  await page.goto("https://www.google.com");
+});
