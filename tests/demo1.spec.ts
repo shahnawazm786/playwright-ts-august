0280dcdf01ae07f60d3f7418de31182bc31cd951
Mohammad Shahnawaz <msnazia2012@gmail.com>
2025-08-17 09:43:11 +0530

    17 Aug 2025

--- tests/demo1.spec.ts
+++ tests/demo1.spec.ts
@@ -1,1 +1,12 @@
-
+import { Browser, chromium, test } from "@playwright/test";
+//import {test} from "@playwright/test"
+test("open browser from local chrome path", async ({}) => {
+  const explorer: Browser = await chromium.launch({
+    headless: false,
+    executablePath:
+      "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe",
+  });
+  const context = await explorer.newContext();
+  const page = await context.newPage();
+  await page.goto("https://www.google.com");
+});
